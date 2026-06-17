e9c474edd5e22e6e6a5ec6e39a93f189b530c8ad
Robin Herbeck <info@robinherbeck.com>
2026-06-17 09:56:59 +0200

    Core 2.3.0 (Suite-Erweiterung), Version 0.2.4

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: backup, database, export, import, migration
 Requires at least: 6.5
 Tested up to: 7.0
 Requires PHP: 8.1
-Stable tag: 0.2.3
+Stable tag: 0.2.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- rh-backup.php
+++ rh-backup.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-backup
  * Update URI:        https://github.com/herbeckrobin/rh-backup
  * Description:       DB-Backup, Export, Import und Restore für WordPress. Pure PHP, Shared-Hosting-tauglich. Teil der rh-blueprint Kollektion.
- * Version:           0.2.3
+ * Version:           0.2.4
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHBACKUP_VERSION', '0.2.3');
+define('RHBACKUP_VERSION', '0.2.4');
 define('RHBACKUP_PLUGIN_FILE', __FILE__);
 define('RHBACKUP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
